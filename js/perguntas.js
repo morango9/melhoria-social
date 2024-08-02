7fb697aced0b4306008c2ead3d3b855f486f7614
morango9 <105944114+morango9@users.noreply.github.com>
2024-08-02 11:10:03 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -11,6 +11,7 @@ const perguntas = [
                 afirmacao: "Sinto muito que pense assim, pois os estudos são importantes. Com eles adquirimos conhecimentos, cultura, e traçamos objetivos na vida.",
             },
         ],
+        proximo: 1;
     },
     {
         enunciado: "Atualmente somente 4.3% dos municípios do Paraná tem Sistema Próprio de Ensino,  95.7% de seus municípios estão vinculados ao sistema estadual de ensino, no qual se compreende que escolas públicas e particulares, devem seguir as diretrizes do Conselho Estadual de Educação.  Você está satisfeito com o método de ensino?",
@@ -24,6 +25,7 @@ const perguntas = [
                 afirmacao: "O Sistema Estadual de Ensino existe para otimizar todas as atividades escolares e oferecer a melhor experiência ao aluno. Com uma estrutura pedagógica completa, que abrange inclusive o material didático.",
             },
         ],
+        proxima : 2,
     },
     {
         enunciado: "Estudos apontam que estudar pode aumentar a capacidade da memória, ativar o poder de concentração e, com o hábito, aumentar a nossa capacidade de aprender. Quanto mais estudamos e exercitamos o cérebro, mais protegido ele estará de doenças de degeneração, como o mal de Alzheimer, por exemplo. Você acredita que estudar faz bem para nossa saúde?",
@@ -37,6 +39,7 @@ const perguntas = [
                 afirmacao: "Existem muitas pesquisas em andamento sobre a relação entre a recuperação de lesões cerebrais e o nível de escolaridade do indivíduo, mas o que já sabemos é que um dos benefícios de estudar é que a reserva cognitiva de quem passou anos e anos estudando é como um cofre. Nele, você vai guardando alguns conhecimentos que serão utilizados em casos de emergência.",
             },
         ],
+        proximo : 3,
     },
     {
         enunciado: "O cansaço constante proveniente de longas sessões de estudo pode levar à exaustão mental. Isso, por sua vez, pode prejudicar a capacidade de concentração e diminuir a motivação para estudar. Qual sua opinião sobre estudar muito?",
@@ -50,6 +53,7 @@ const perguntas = [
                 afirmacao: "É importante lembrar novamente que exagerar na quantidade de horas de estudo por dia pode causar exaustão e prejudicar seu desempenho durante a prova. Dessa forma, há uma média de quatro horas de estudo diário recomendada a fim de que você não se canse mentalmente.",
             },
         ],
+        proximo : 4,
     },
     {
         enunciado: "Sem uma rotina de estudos, pode ficar difícil reter uma grande quantidade de conteúdos. Então, uma das principais formas de superar a dificuldade de aprender é por meio de um planejamento diário. O que você faz quando está com dificuldade na aprendizagem?",
