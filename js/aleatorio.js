3fdb6aebbe7685483da070a0a4435c19aee4946a
morango9 <105944114+morango9@users.noreply.github.com>
2024-08-09 11:10:40 -0300

    Add files via upload

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,8 +1,8 @@
 const nomes = ["Fernanda" , "Giuliana", "Maria Eduarda" , "Marcelo" , "Amanda" , "Gustavo" , "Adrieli"];
 
-export function aleatorio (lista){
-    const posicao = Math.floor(math.random()* lista.length);
+function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
     return lista[posicao];
 }
 
-export const nome= aleatorio(nomes);
+const nome = aleatorio(nomes);
