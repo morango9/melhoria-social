a0543121d590cf812b109ed8c657314e8e8796ca
morango9 <105944114+morango9@users.noreply.github.com>
2024-08-30 07:54:17 -0300

    Delete aleatorio.js

--- aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["Fernanda" , "Giuliana", "Maria Eduarda" , "Marcelo" , "Amanda" , "Gustavo" , "Adrieli"];
-
-function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-const nome = aleatorio(nomes);
